4c0c58df0dbad62caf253669e65d96ff96e6e122
ChrisCisneros <chcisneros@csumb.edu>
2021-09-09 15:36:12 -0700

    Added cursor and clickable buttons

--- New Unity Project/Assets/MouseLook.cs
+++ New Unity Project/Assets/MouseLook.cs
@@ -26,5 +26,30 @@ public class MouseLook : MonoBehaviour
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
 
         playerBody.Rotate(Vector3.up * mouseX);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.tag == "Button")
+                {
+                    Debug.Log("Hit");
+                    hit.transform.gameObject.SetActive(false);
+                    /*Vector3 pos = hit.transform.position;
+                    Quaternion rot = Quaternion.Euler(0, 0, 0);
+                    Instantiate(prefab, pos, Quaternion.identity, World.transform);*/
+
+
+                }
+                else
+                {
+                    print("nothing here!");
+                }
+            }
+
+        }
     }
 }
